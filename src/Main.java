4a92a745f5106a9bb1df9d1728411533e52341e8
O Tal do Ricardo <33401083+ricardo19000@users.noreply.github.com>
2025-06-03 20:54:32 -0300

    Primeira opção - if

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,22 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-    System.out.println("Ajuda com:");
-    System.out.println("    1. if");
-    System.out.println("    2. switch");
+    public static void main(String[] args)
+        throws java.io.IOException {
+            char escolha;
+
+            System.out.println("Ajuda com:");
+            System.out.println("1. if");
+            System.out.println("2. switch");
+
+            escolha = (char) System.in.read();
+
+            switch (escolha) {
+                case '1':
+                    System.out.println("O if:\n");
+                    System.out.println("if (condição) instrução;");
+                    System.out.println("else instrução;");
+                    break;
+            }
+        }
     }
-}
