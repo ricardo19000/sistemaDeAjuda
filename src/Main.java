feefd6849a39498c2ab96d759958c92c0fe81659
O Tal do Ricardo <33401083+ricardo19000@users.noreply.github.com>
2025-06-03 22:53:10 -0300

    Melhoria no sistema de menu

--- src/Main.java
+++ src/Main.java
@@ -9,54 +9,70 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Ajuda com:");
-        System.out.println("1. if");
-        System.out.println("2. switch");
-        System.out.println("3. for");
-        System.out.println("4. while");
-        System.out.println("5. do-while\n");
-        System.out.println("Escolha um número ou digite o operador: ");
+        while(true) {
+            System.out.println("Ajuda com:\n");
+            System.out.println("1. if");
+            System.out.println("2. switch");
+            System.out.println("3. for");
+            System.out.println("4. while");
+            System.out.println("5. do-while");
+            System.out.println("0. Sair");
+            System.out.print("\nEscolha um número ou digite o operador: ");
 
-        escolha = sc.next();
+            escolha = sc.next();
 
-        switch (escolha) {
-            case "1", "if":
-                System.out.println("O if:\n");
-                System.out.println("if (Condição) Instrução;");
-                System.out.println("else Instrução;");
+            if (escolha.equals("0")){
+                System.out.print("Até logo...");
                 break;
+            }
 
-            case "2", "switch":
-                System.out.println("O Switch:\n");
-                System.out.println("Switch (Expressão) {");
-                System.out.println("    case Constante:");
-                System.out.println("        Código");
-                System.out.println("        break;");
-                System.out.println("    default:");
-                System.out.println("        System.out.print(Mensagem de erro/exceção)");
-                System.out.println("}");
-                break;
+            boolean validacao = true;
 
-            case "3", "for":
-                System.out.println("O for:\n");
-                System.out.println("for(Init; Condição; Instrução)");
-                System.out.println("Código;");
-                break;
+            switch (escolha) {
+                case "1", "if":
+                    System.out.println("O if:\n");
+                    System.out.println("if (Condição) Instrução;");
+                    System.out.println("else Instrução;");
+                    break;
 
-            case "4", "while":
-                System.out.println("O While:\n");
-                System.out.println("while (Condição) Instrução;");
-                break;
+                case "2", "switch":
+                    System.out.println("O Switch:\n");
+                    System.out.println("Switch (Expressão) {");
+                    System.out.println("    case Constante:");
+                    System.out.println("        Código");
+                    System.out.println("        break;");
+                    System.out.println("    default:");
+                    System.out.println("        System.out.print(Mensagem de erro/exceção)");
+                    System.out.println("}");
+                    break;
 
-            case "5", "do-while":
-                System.out.println("O do-while:\n");
-                System.out.println("do {");
-                System.out.println("    Código");
-                System.out.println("} while (condição);");
-                break;
+                case "3", "for":
+                    System.out.println("O for:\n");
+                    System.out.println("for(Init; Condição; Instrução)");
+                    System.out.println("Código;");
+                    break;
 
-            default:
-                System.out.print("Seleção não encontrada");
+                case "4", "while":
+                    System.out.println("O While:\n");
+                    System.out.println("while (Condição) Instrução;");
+                    break;
+
+                case "5", "do-while":
+                    System.out.println("O do-while:\n");
+                    System.out.println("do {");
+                    System.out.println("    Código");
+                    System.out.println("} while (condição);");
+                    break;
+
+                default:
+                    System.out.print("Seleção não encontrada");
+                    validacao = false;
+            }
+            if(validacao){
+                System.out.println("\nAperte enter para continuar");
+                sc.nextLine();
+                sc.nextLine();
+            }
         }
     }
 }
