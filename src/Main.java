e3f391aeff48b5e633e1cdbb02d999809f699ab5
O Tal do Ricardo <33401083+ricardo19000@users.noreply.github.com>
2025-06-03 22:02:47 -0300

    Mais orientações

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,23 @@
 public class Main {
     public static void main(String[] args)
         throws java.io.IOException {
-            char escolha;
+            char escolha, ignorar;
 
-            System.out.println("Ajuda com:");
-            System.out.println("1. if");
-            System.out.println("2. switch");
+            do {
+                System.out.println("Ajuda com:");
+                System.out.println("1. if");
+                System.out.println("2. switch");
+                System.out.println("3. for");
+                System.out.println("4. while");
+                System.out.println("5. do while\n");
+                System.out.println("Escolha um: ");
 
-            escolha = (char) System.in.read();
+                escolha = (char) System.in.read();
+
+                do {
+                    ignorar = (char) System.in.read();
+                } while (ignorar != '\n');
+            } while( escolha < '1' | escolha > '5' );
 
             switch (escolha) {
                 case '1':
@@ -21,12 +31,31 @@ public class Main {
                     System.out.println("O Switch:\n");
                     System.out.println("Switch (Expressão) {");
                     System.out.println("    case Constante:");
-                    System.out.println("        Instrução");
+                    System.out.println("        Código");
                     System.out.println("        break;");
                     System.out.println("    default:");
                     System.out.println("        System.out.print(Mensagem de erro/exceção)");
                     System.out.println("}");
                     break;
+
+                case '3':
+                    System.out.println("O for:\n");
+                    System.out.println("for(Init; Condição; Instrução)");
+                    System.out.println("Código;");
+                    break;
+
+                case '4':
+                    System.out.println("O While:\n");
+                    System.out.println("while (Condição) Instrução;");
+                    break;
+
+                case '5':
+                    System.out.println("O do-while:\n");
+                    System.out.println("do {");
+                    System.out.println("    Código");
+                    System.out.println("} while (condição);");
+                    break;
+
                 default:
                     System.out.print("Seleção não encontrada");
             }
